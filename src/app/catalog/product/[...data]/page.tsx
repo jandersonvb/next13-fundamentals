ea30cc3b985f4f45287e24ff16c7804fe52f737a
jandersonvb <jandersonvb.dev@gmail.com>
2024-01-19 17:07:59 -0300

    feat: fetch data from components

--- src/app/catalog/product/[...data]/page.tsx
+++ src/app/catalog/product/[...data]/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 interface IProductProps {
   params: {
     data: string[]
@@ -6,11 +8,19 @@ interface IProductProps {
 
 export default function Product({ params }: IProductProps) {
   const [productId, size, color] = params.data
+
+  console.log(params)
+
+  function AddToCart() {
+    console.log('Add to cart')
+  }
+
   return (
     <div>
       <h1>Product {productId}</h1>
       <h2>Size: {size}</h2>
       <h2>Color: {color}</h2>
+      <button onClick={AddToCart}>Enviar para o carrinho</button>
     </div>
   )
 }
